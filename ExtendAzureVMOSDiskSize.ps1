4fab64823e5ac0a192fc41040012796fc4b42df3
unknown <steve.k.boyd@ontario.ca>
2017-08-18 09:36:42 -0400

    Initial commit

---
+++ ExtendAzureVMOSDiskSize.ps1
@@ -0,0 +1,9 @@
+﻿Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId YOUR_SUBSCRIPTION_GUID
+$rgName = 'YOUR_RESOURCE_GROUP_NAME'
+$vmName = 'YOUR_VM_NAME'
+$vm = Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+Stop-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+$vm.StorageProfile.OSDisk.DiskSizeGB = 1023
+Update-AzureRmVM -ResourceGroupName $rgName -VM $vm
+Start-AzureRmVM -ResourceGroupName $rgName -Name $vmName
