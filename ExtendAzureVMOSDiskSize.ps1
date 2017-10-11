08ea4bc5ddbfef8e06a756995efa5c2cb0413bf7
Steve Boyd <steve@steveboyd.me>
2017-10-11 11:45:43 -0400

    Update ExtendAzureVMOSDiscSize.ps1
    Remove and attach os disc from vm

--- ExtendAzureVMOSDiskSize.ps1
+++ ExtendAzureVMOSDiskSize.ps1
@@ -2,8 +2,12 @@
 Select-AzureRmSubscription -SubscriptionId YOUR_SUBSCRIPTION_GUID
 $rgName = 'YOUR_RESOURCE_GROUP_NAME'
 $vmName = 'YOUR_VM_NAME'
+$vmOsDisc = 'YOUR_VM_OS_DISC'
 $vm = Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName
-Stop-AzureRmVM -ResourceGroupName $rgName -Name $vmName
 $vm.StorageProfile.OSDisk.DiskSizeGB = 1023
+
+Stop-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+Remove-AzureRmVMDataDisk -VM $vm -Name $vmOsDisc
 Update-AzureRmVM -ResourceGroupName $rgName -VM $vm
+Add-AzureRmVMDataDisk -VM $vm -Name $vmOsDisc
 Start-AzureRmVM -ResourceGroupName $rgName -Name $vmName
